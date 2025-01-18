20c9308a7b8802de33c46ca38222751676039452
Safal Neupane <safal.npane@gmail.com>
2025-01-18 12:51:09 +1100

    feat: register setenv and getenv to lua

--- src/lua.c
+++ src/lua.c
@@ -8,22 +8,74 @@
 
 lua_State *L = NULL;
 
+// Register C functions
+luaL_Reg nish_functions[] = {
+	{"getenv", lua_getenv },
+	{"setenv", lua_setenv },
+	{NULL, NULL },
+};
+
+
 
 void
 initialize_lua()
 {
 	L = luaL_newstate();
 	luaL_openlibs(L);  // Load lua standard libraries
+	
+	setup_nish_obj(L);
 }
 
 
-int execute_lua_inline(const char *script) {
+int
+execute_lua_inline(const char *script)
+{
     if (luaL_dostring(L, script) != LUA_OK) {
         fprintf(stderr, "Lua error: %s\n", lua_tostring(L, -1));
         lua_pop(L, 1);
         return 1;
     }
     return 0;
+}
+
+
+int
+lua_getenv(lua_State *L)
+{
+	const char *var = luaL_checkstring(L, 1);
+	const char *value = getenv(var);
+	
+	if (value)
+		lua_pushstring(L, value);
+	else
+		lua_pushnil(L);
+
+	return 1;
+}
+
+
+int
+lua_setenv(lua_State *L)
+{
+	const char *var = luaL_checkstring(L, 1);
+	const char *val = luaL_checkstring(L, 2);
+	int overwrite = luaL_optinteger(L, 3, 1);
+	
+	if (setenv(var, val, overwrite) == 0)
+		lua_pushboolean(L, 1);
+	else
+		lua_pushboolean(L, 0);
+
+	return 1;
+}
+
+
+void
+setup_nish_obj(lua_State *L)
+{
+	lua_newtable(L);
+	luaL_setfuncs(L, nish_functions, 0);
+	lua_setglobal(L, "nish");
 }
 
 
--- src/lua.h
+++ src/lua.h
@@ -4,12 +4,18 @@
 #ifndef LUA_H
 #define LUA_H
 
+#include <lua.h>
 #include <lualib.h>
+#include <lauxlib.h>
 
 
 void initialize_lua();
 void close_lua();
 int execute_lua_inline(const char *script);
 
+int lua_getenv(lua_State *L);
+int lua_setenv(lua_State *L);
+
+void setup_nish_obj(lua_State *L);
 
 #endif
